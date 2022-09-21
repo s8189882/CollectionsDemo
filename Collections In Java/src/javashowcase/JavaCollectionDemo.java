feb651407dc96a0d8d813c33ddb225d80fc02f47
s8189882 <s8189882@gmail.com>
2022-09-21 15:49:31 +0530

    Add UC2 - stack demo

--- Collections In Java/src/javashowcase/JavaCollectionDemo.java
+++ Collections In Java/src/javashowcase/JavaCollectionDemo.java
@@ -3,15 +3,35 @@ package javashowcase;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class JavaCollectionDemo {
 	
 	public static void main(String[] args) {
 		doListDemo();
+		doStackDemo();
+	}
+
+	private static void doStackDemo() {
+		System.out.println("\nIn doStackDemo\n");	
+		
+		Stack<String> stack = new Stack<>();
+		
+		stack.push("Ayush");
+		stack.push("Garvit");
+		stack.push("Amit");
+		stack.push("Ashish");
+		stack.push("Garima");
+		
+		String pop = stack.pop();
+		Iterator<String> itr = stack.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 	}
 
 	private static void doListDemo() {
-		System.out.println("In dolistDemo");
+		System.out.println("\nIn dolistDemo\n");
 		
 		//creating List
 		List<String> list = new LinkedList<>();
