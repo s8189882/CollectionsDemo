e9ef570b10310b0cc458641876b3fa35b0607c6b
s8189882 <s8189882@gmail.com>
2022-09-21 15:42:00 +0530

    create master

---
+++ Collections In Java/src/javashowcase/JavaCollectionDemo.java
@@ -0,0 +1,12 @@
+package javashowcase;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaCollectionDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("Collection in Java Demo");
+	}
+}
