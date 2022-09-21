b5ab596a7df3be639e24899391fe80a09a751bcd
s8189882 <s8189882@gmail.com>
2022-09-21 15:43:01 +0530

    Add UC1 - list demo

--- Collections In Java/src/javashowcase/JavaCollectionDemo.java
+++ Collections In Java/src/javashowcase/JavaCollectionDemo.java
@@ -7,6 +7,25 @@ import java.util.List;
 public class JavaCollectionDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("Collection in Java Demo");
+		doListDemo();
+	}
+
+	private static void doListDemo() {
+		System.out.println("In dolistDemo");
+		
+		//creating List
+		List<String> list = new LinkedList<>();
+		
+		//adding objects to list
+		list.add("Ravi");
+		list.add("Vijay");
+		list.add("Ravi");
+		list.add("Ajay");
+		
+		//Traversing list through Iterator
+		Iterator itr = list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 	}
 }
